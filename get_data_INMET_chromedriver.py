8194f52a58009aa5e9e4e7651dc069f42aaea2a0
MarceloCapeletti <marcelocapeletti@gmail.com>
2021-07-26 08:24:33 -0300

    Mudanças de nomeclatura

--- get_data_INMET_chromedriver.py
+++ get_data_INMET_chromedriver.py
@@ -9,10 +9,11 @@ import pandas as pd
 #   Autor: Marcelo Bruno Capeletti
 
 class importDataInmet:
-    def __init__(self,url,dataInicio,dataFinal):
+    
+    def __init__(self,url,dayIni,dayEnd):
         self.url = url
-        self.dataInicio = dataInicio
-        self.dataFinal = dataFinal
+        self.dayIni = dayIni
+        self.dayEnd = dayEnd
         
     def import_(self):
        
@@ -21,40 +22,46 @@ class importDataInmet:
         driver.set_window_size(1024, 1024)                  #Utiliza resolução padrão afim de evitar erros
         #driver.set_window_position(0, -1000)               #Retira navegador da tela
         time.sleep(1)
-        driver.get(self.url)                                #informa a url
+        driver.get(self.url)                                #Informa a url para biblioteca
         time.sleep(5)                                       #Aguarda carregamento da pagina
         driver.find_element_by_id("menu").click()           #Encontra o elemento Menu Canto direito
         DataInicio = driver.find_element_by_id("datepicker_EstacoesTabela_Inicio")        #Encontra Box(do site) afim de inserir data inicial
         #print(DataInicio)
         time.sleep(1)                                       #Aguarda carregamento da pagina
         DataInicio.clear()                                  #Retira dados da box
-        DataInicio.send_keys(self.dataInicio)               #Insere dados no box
+        DataInicio.send_keys(self.dayIni)                   #Insere dados no box
         time.sleep(1)                                       #Aguarda carregamento da pagina
         #driver.quit()
         DataFim = driver.find_element_by_id("datepicker_EstacoesTabela_Fim")  #Encontra Box que irá a data final
         time.sleep(1)                                       #Tempo espera
         DataFim.clear()                                     #Retira dados da box
-        DataFim.send_keys(self.dataFinal)                   #Insere dados no box
+        DataFim.send_keys(self.dayEnd)                      #Insere dados no box
         time.sleep(1)                                       #Aguarda carregamento da pagina
         driver.find_element_by_id("menu").click()
         time.sleep(1)                                          
         driver.find_element_by_id("EstacoesTabela").click() #Clica no botao 'Gerar Tabela'(da pagina)
         time.sleep(5)                                       #Aguarda carregamento da pagina e dos dados
-        all_tables = pd.read_html(driver.page_source, attrs={'id': 'tabela'}) 	           #Encontra tabela a ser importada
+        all_tables = pd.read_html(driver.page_source, attrs={'id': 'tabela'}) #Encontra tabela a ser importada
         global df
         df = all_tables[0]                                  #Tabela da pagina para dataframe
-        driver.quit()                                       #Fecha Chrome
+        driver.quit()                                       #Encerra Chrome
+                                    
     def getdata(self):
+        
         return df
+    
     def printdata(self):
+        
          print(df)              #Imprime tabela
+         
     def datatocsv(self):
+        
          df.to_csv('dados.csv') #Exporta Tabela
 
-dataInicio = input('Data de início da Importação DD/MM/AAAA \n')   #Solicita ao usuário data de início
-dataFinal = input('Data de início da Importação DD/MM/AAAA \n ')   #Solicita ao usuário data de fim
+dayIni = input('Data de início da Importação DD/MM/AAAA \n')   #Solicita ao usuário data de início
+dayEnd = input('Data de início da Importação DD/MM/AAAA \n ')   #Solicita ao usuário data de fim
   
-importDataInmet = importDataInmet('https://tempo.inmet.gov.br/TabelaEstacoes/A801',dataInicio,dataFinal)
+importDataInmet = importDataInmet('https://tempo.inmet.gov.br/TabelaEstacoes/A801',dayIni,dayEnd)
 importDataInmet.import_()
 df = importDataInmet.getdata()
 importDataInmet.printdata()
