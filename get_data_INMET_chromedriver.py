35f7a5ba9cb75d085fce69ce00b13f635a025080
MarceloCapeletti <marcelocapeletti@gmail.com>
2021-07-26 00:34:22 -0300

    Criada classe importDataInmet Definido funções da classe import_,getdata,printdata,datatocsv.

--- get_data_INMET_chromedriver.py
+++ get_data_INMET_chromedriver.py
@@ -2,45 +2,60 @@ from selenium import webdriver
 import time as time
 import pandas as pd
 
-#       Importação dados meteorologicos 
-#       do site INMET. Pede url do site INMET e data de começo e fim de importação 
-#       No final é criado o arquivo dados.csv 
-#       Versão 1.0
+#   Bot para importar dados automaticamente do INMET (Instituto Nacional de Meteorologia) 
+#   Precisa URL da estação que se deseja importar do INMET
+#   e pede data de inicio e final da importação 
+#   Versão 1.1
+#   Autor: Marcelo Bruno Capeletti
 
-def getDadosInmet (url,DataInicioDoUsuario,DataFimDoUsuario):
-   
-    options = webdriver.ChromeOptions()                 #inicia
-    driver = webdriver.Chrome(chrome_options=options)
-    driver.set_window_size(1024, 1024)                    #mesma resolução, evitar erros
-    #driver.set_window_position(0, -1000)                #esconde Chrome do usuário
-    time.sleep(1)
-    driver.get(url)                                     #Carrega o HTML
-    time.sleep(5)                                       #Tempo para carregar a pagina
-    driver.find_element_by_id("menu").click()           #Encontra o elemento Menu Canto direito
-    DataInicio = driver.find_element_by_id("datepicker_EstacoesTabela_Inicio")        #Encontra Box(do site) que irá a data inicial
-   # print(DataInicio)
-    time.sleep(1)                                       #Tempo espera do Chrome
-    DataInicio.clear()                                  #Tira dados da box
-    DataInicio.send_keys(DataInicioDoUsuario)           #Coloca dados no box
-    time.sleep(1)                                       #Tempo espera do Chrome
-   # driver.quit()
-    DataFim = driver.find_element_by_id("datepicker_EstacoesTabela_Fim")  #Encontra Box que irá a data final
-    time.sleep(1)                                       #Tempo espera
-    DataFim.clear()                                     #Tira dados da box
-    DataFim.send_keys(DataFimDoUsuario)                 #Coloca dados no box
-    time.sleep(1)                                       #Tempo espera do Chrome
-    driver.find_element_by_id("menu").click()
-    time.sleep(1)                                          
-    driver.find_element_by_id("EstacoesTabela").click() #Clica botao 'Gerar Tabela'(do site)
-    time.sleep(5)                                       #Espera para carregar tabela
-    all_tables = pd.read_html(driver.page_source, attrs={'id': 'tabela'}) #Encontra tabela a ser importada
-    df = all_tables[0]
-    print(df)                                           #Imprime tabela
-    df.to_csv('dados.csv') #Exporta Tabela
-    driver.quit()                                       #Fecha Chrome
+class importDataInmet:
+    def __init__(self,url,dataInicio,dataFinal):
+        self.url = url
+        self.dataInicio = dataInicio
+        self.dataFinal = dataFinal
+        
+    def import_(self):
+       
+        options = webdriver.ChromeOptions()                 #start
+        driver = webdriver.Chrome(chrome_options=options)
+        driver.set_window_size(1024, 1024)                   #Utiliza resolução padrão afim de evitar erros
+        #driver.set_window_position(0, -1000)                #Retira navegador da tela
+        time.sleep(1)
+        driver.get(self.url)                                #informa a url
+        time.sleep(5)                                       #Aguarda carregamento da pagina
+        driver.find_element_by_id("menu").click()           #Encontra o elemento Menu Canto direito
+        DataInicio = driver.find_element_by_id("datepicker_EstacoesTabela_Inicio")        #Encontra Box(do site) afim de inserir data inicial
+       # print(DataInicio)
+        time.sleep(1)                                       #Aguarda carregamento da pagina
+        DataInicio.clear()                                  #Retira dados da box
+        DataInicio.send_keys(self.dataInicio)               #Insere dados no box
+        time.sleep(1)                                       #Aguarda carregamento da pagina
+       # driver.quit()
+        DataFim = driver.find_element_by_id("datepicker_EstacoesTabela_Fim")  #Encontra Box que irá a data final
+        time.sleep(1)                                       #Tempo espera
+        DataFim.clear()                                     #Retira dados da box
+        DataFim.send_keys(self.dataFinal)                   #Insere dados no box
+        time.sleep(1)                                       #Aguarda carregamento da pagina
+        driver.find_element_by_id("menu").click()
+        time.sleep(1)                                          
+        driver.find_element_by_id("EstacoesTabela").click() #Clica no botao 'Gerar Tabela'(da pagina)
+        time.sleep(5)                                       #Aguarda carregamento da pagina e dos dados
+        all_tables = pd.read_html(driver.page_source, attrs={'id': 'tabela'}) #Encontra tabela a ser importada
+        global df
+        df = all_tables[0]                                  #Tabela da pagina para dataframe
+        driver.quit()                                       #Fecha Chrome
+    def getdata(self):
+        return df
+    def printdata(self):
+         print(df)              #Imprime tabela
+    def datatocsv(self):
+         df.to_csv('dados.csv') #Exporta Tabela
 
-DataInicio = input('Data de início da Importação DD/MM/AAAA \n') #Pede usuário data de início
-DataFim = input('Data de início da Importação DD/MM/AAAA \n ')   #Pede ao usuário data de fim
+dataInicio = input('Data de início da Importação DD/MM/AAAA \n')   #Solicita ao usuário data de início
+dataFinal = input('Data de início da Importação DD/MM/AAAA \n ')   #Solicita ao usuário data de fim
   
-
-getDadosInmet('https://tempo.inmet.gov.br/TabelaEstacoes/A801',DataInicio,DataFim) #Inicia funçao importação
+importDataInmet = importDataInmet('https://tempo.inmet.gov.br/TabelaEstacoes/A801',dataInicio,dataFinal)
+importDataInmet.import_()
+df = importDataInmet.getdata()
+importDataInmet.printdata()
+importDataInmet.datatocsv()
