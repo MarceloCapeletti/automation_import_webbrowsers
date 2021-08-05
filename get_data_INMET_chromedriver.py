0d7d15bf4788578b8f47cd12f5fa70a30004a1ff
MarceloCapeletti <marcelocapeletti@gmail.com>
2021-08-05 19:04:51 -0300

    Adicionado Tratamento de erro

--- get_data_INMET_chromedriver.py
+++ get_data_INMET_chromedriver.py
@@ -6,6 +6,7 @@ import datetime
 #   Bot para importar dados automaticamente do INMET (Instituto Nacional de Meteorologia) 
 #   Precisa URL da estação que se deseja importar do INMET
 #   e pede data de inicio e final da importação 
+#   chromedriver.exe no diretório https://chromedriver.chromium.org/downloads
 #   Versão 1.1
 #   Autor: Marcelo Bruno Capeletti
 
@@ -18,35 +19,45 @@ class importDataInmet:
         self.df = pd.DataFrame()
         
     def import_(self):
-       
-        options = webdriver.ChromeOptions()                 #start
-        driver = webdriver.Chrome(chrome_options=options)
-        driver.set_window_size(1024, 1024)                  #Utiliza resolução padrão afim de evitar erros
-        #driver.set_window_position(0, -1000)               #Retira navegador da tela
-        time.sleep(1)
-        driver.get(self.url)                                #Informa a url para biblioteca
-        time.sleep(5)                                       #Aguarda carregamento da pagina
-        driver.find_element_by_id("menu").click()           #Encontra o elemento Menu Canto direito
-        DataInicio = driver.find_element_by_id("datepicker_EstacoesTabela_Inicio")        #Encontra Box(do site) afim de inserir data inicial
-        #print(DataInicio)
-        time.sleep(1)                                       #Aguarda carregamento da pagina
-        DataInicio.clear()                                  #Retira dados da box
-        DataInicio.send_keys(self.dayIni)                   #Insere dados no box
-        time.sleep(1)                                       #Aguarda carregamento da pagina
-        #driver.quit()
-        DataFim = driver.find_element_by_id("datepicker_EstacoesTabela_Fim")  #Encontra Box que irá a data final
-        time.sleep(1)                                       #Tempo espera
-        DataFim.clear()                                     #Retira dados da box
-        DataFim.send_keys(self.dayEnd)                      #Insere dados no box
-        time.sleep(1)                                       #Aguarda carregamento da pagina
-        driver.find_element_by_id("menu").click()
-        time.sleep(1)                                          
-        driver.find_element_by_id("EstacoesTabela").click() #Clica no botao 'Gerar Tabela'(da pagina)
-        time.sleep(5)                                       #Aguarda carregamento da pagina e dos dados
-        all_tables = pd.read_html(driver.page_source, attrs={'id': 'tabela'}) #Encontra tabela a ser importada
-        self.df = all_tables[0]                             #Tabela da pagina para dataframe
-        driver.quit()                                       #Encerra Chrome
-                                    
+        
+        try:
+     
+            options = webdriver.ChromeOptions()                 #start
+            driver = webdriver.Chrome(chrome_options=options)
+            driver.set_window_size(1024, 1024)                  #Utiliza resolução padrão afim de evitar erros
+            #driver.set_window_position(0, -1000)               #Retira navegador da tela
+            time.sleep(1)
+            driver.get(self.url)                                #Informa a url para biblioteca
+            time.sleep(5)                                       #Aguarda carregamento da pagina
+            driver.find_element_by_id("menu").click()           #Encontra o elemento Menu Canto direito
+            DataInicio = driver.find_element_by_id("datepicker_EstacoesTabela_Inicio")        #Encontra Box(do site) afim de inserir data inicial
+            #print(DataInicio)
+            time.sleep(1)                                       #Aguarda carregamento da pagina
+            DataInicio.clear()                                  #Retira dados da box
+            DataInicio.send_keys(self.dayIni)                   #Insere dados no box
+            time.sleep(1)                                       #Aguarda carregamento da pagina
+            #driver.quit()
+            DataFim = driver.find_element_by_id("datepicker_EstacoesTabela_Fim")  #Encontra Box que irá a data final
+            time.sleep(1)                                       #Tempo espera
+            DataFim.clear()                                     #Retira dados da box
+            DataFim.send_keys(self.dayEnd)                      #Insere dados no box
+            time.sleep(1)                                       #Aguarda carregamento da pagina
+            driver.find_element_by_id("menu").click()
+            time.sleep(1)                                          
+            driver.find_element_by_id("EstacoesTabela").click() #Clica no botao 'Gerar Tabela'(da pagina)
+            time.sleep(5)                                       #Aguarda carregamento da pagina e dos dados
+            all_tables = pd.read_html(driver.page_source, attrs={'id': 'tabela'}) #Encontra tabela a ser importada
+            self.df = all_tables[0]                             #Tabela da pagina para dataframe
+        
+        except Exception as err:
+            
+            print("Erro! Começar novamente a importação!")
+            print(err);
+            
+        finally:
+
+            driver.quit()
+
     def getdata(self):
         
         return self.df
@@ -59,27 +70,21 @@ class importDataInmet:
         
          self.df.to_csv('dados.csv')                            #Exporta Tabela
 
-class Error(Exception):
-    pass
 
-class InputError(Error):
-    
-    def __init__(self,message):
-        
-        self.message = message
-        
-while True:
-    try:
-        dayIni = input('Data de início da Importação DD/MM/AAAA \n')    #Solicita ao usuário data de início
-        datetime.datetime.strptime(dayIni, '%d/%m/%Y')
-        dayEnd = input('Data de fim da Importação DD/MM/AAAA \n ')      #Solicita ao usuário data de fim
-        datetime.datetime.strptime(dayEnd, '%d/%m/%Y')
-        importDataInmet = importDataInmet('https://tempo.inmet.gov.br/TabelaEstacoes/A801',dayIni,dayEnd) #Para outra estação, modificar URL
-        importDataInmet.import_()
-        df = importDataInmet.getdata()
-        importDataInmet.printdata()
-        importDataInmet.datatocsv()
-        break
-    except ValueError as ex:
-        print(str(ex) + '\nValor invalido. Digite novamente:')
+if __name__ == '__main__':  
+      
+    while True:
+        try:
+            dayIni = input('Data de início da Importação DD/MM/AAAA \n')    #Solicita ao usuário data de início
+            datetime.datetime.strptime(dayIni, '%d/%m/%Y')
+            dayEnd = input('Data de fim da Importação DD/MM/AAAA \n ')      #Solicita ao usuário data de fim
+            datetime.datetime.strptime(dayEnd, '%d/%m/%Y')
+            importDataInmet = importDataInmet('https://tempo.inmet.gov.br/TabelaEstacoes/A801',dayIni,dayEnd) #Para outra estação, modificar URL
+            importDataInmet.import_()
+            df = importDataInmet.getdata()
+            importDataInmet.printdata()
+            importDataInmet.datatocsv()
+            break
+        except ValueError as ex:
+            print(str(ex) + '\nValor invalido. Digite novamente:')
 
